dfd15b2f95480f8d3a692401da425cf6e45c3d94
Sivasamy <sivasamyboxy18@gmail.com>
2026-01-01 13:45:59 +0530

    customer table seeder create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            BeatSeeder::class,
+            CustomerSeeder::class,
         ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
