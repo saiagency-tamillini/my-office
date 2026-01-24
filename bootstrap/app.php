4b63be7d7cd27fd2c880289d3ab4022fb311d7bf
Sivasamy <sivasamyboxy18@gmail.com>
2026-01-25 01:47:37 +0530

    env changed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo(fn () => route('home'));
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
