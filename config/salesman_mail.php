f999b732ed030b08ede46650732782efd61ffbcd
saiagency <saiagency0625@gmail.com>
2026-05-16 19:32:35 +0530

    maild id's corrected

--- config/salesman_mail.php
+++ config/salesman_mail.php
@@ -2,7 +2,7 @@
 
 return [
     'BALA' => 'balanagul0219@gmail.com',
-    'SHRI MAHESHWARAN' => 'saiagency0625@gmail.com',
+    'SHRI MAHESHWARAN' => 'srimaheshkn@gmail.com',
     'NAGARAJ' => 'Sathishsingaram@gmail.com',
-    'ASHIK' => 'srimaheshkn@gmail.com',
+    'ASHIK' => 'asikmohammed2222@gmail.com',
 ];
